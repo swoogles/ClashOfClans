1d792cf3db01a5b802da3a7f7ebf725dd841c133
Bill Frasure <bill.frasure@gmail.com>
2014-05-27 14:52:15 -0600

    Start accessing unit cassandra table

--- armyplanner_py2.py
+++ armyplanner_py2.py
@@ -4,6 +4,7 @@
 #   """
 
 import random
+from units_specific import Barbarian, Archer
 from numpy import array,arange
 from cassandra.cluster import Cluster
 
@@ -30,121 +31,7 @@ class Barracks():
 def double(target):
   return target*2
 
-class Unit():
-
-  def __init__(self, level=1):
-    self.hp_max = self._prop_levels_hp[level]
-    self.hp_cur = self.hp_max 
-    self.dps = self._prop_levels_dps[level]
-    self.cost = self._prop_levels_cost[level]
-    # self.pos = (random.random((0,40)),random.random((0,40)))
-
-  def printStats(self):
-    print "Unit Type: ", self.name
-    print "  hp: ", self.hp_cur
-    print "  dps: ", self.dps
-    print "  cost: ", self.cost
-    print ""
-
-  def printHpCur(self):
-    print "HpCur: ", self.hp_cur
-
-
-  def mapHpLevels(self):
-    newmap = map(double, self._prop_levels_hp.values() )
-    print self._prop_levels_hp
-    # Python 2 way 
-    # print *newmap
-    # Python 3 way 
-    #print(*newmap)
-
-  def printCostLevels(self):
-    self.printLevels( self._prop_levels_cost )
-
-  def printLevels(self, prop_levels):
-    print "Values: ", prop_levels.values()
-
-  def attack(self):
-    self._target.hp_cur -= self.dps
-    if ( self._target.isAlive() == False ):
-      del self._target
-
-  def kill(self):
-    while hasattr( self, '_target' ):
-      self._target.printHpCur()
-      self.attack()
-
-
-  def isAlive(self):
-    return ( self.hp_cur > 0 )
-
-  def setTarget(self, target):
-    self._target = target
-
-  def getTarget(self):
-    return self._target
-
-  def hasTarget(self):
-    return hasattr( self, '_target' )
   
-class Barbarian(Unit):
-  name = "Barbarian"
-  _range = 1
-  _prop_levels_hp = { 
-      1:45,
-      2:54, 
-      3:65,
-      4:78,
-      5:95,
-      6:110
-  }
-
-  _prop_levels_dps = { 
-      1:8,
-      2:11, 
-      3:14,
-      4:18,
-      5:23,
-      6:26,
-  }
-
-  _prop_levels_cost = { 
-      1:25,
-      2:40, 
-      3:60,
-      4:80,
-      5:100,
-      6:150
-  }
-
-  _prop_levels_cost_list = [ 
-      25,
-      40, 
-
-      60,
-      80,
-      100,
-      150
-  ]
-
-class Archer(Unit):
-  name = "Archer"
-  _range = 5
-  _prop_levels_hp = { 
-      2:23, 
-      3:28,
-  }
-
-  _prop_levels_dps = { 
-      2:9, 
-      3:12,
-  }
-
-  _prop_levels_cost = { 
-      2:40, 
-      3:80,
-  }
-
 class Battle:
   _units = []
 
@@ -208,6 +95,7 @@ session = cluster.connect('demo')
 jsonNames = {'last_name':'Rob' }
 
 query = "SELECT * FROM users WHERE lastname=%(last_name)s"
+query = "SELECT * FROM unit"
 # future = session.execute_async(query, [last_names])
 
 futures = []
@@ -216,10 +104,12 @@ futures = []
 #   futures.append(session.execute_async( query, [last_name] ))
 
 # for last_name in last_names:
+# futures.append(session.execute_async( query, jsonNames ))
 futures.append(session.execute_async( query, jsonNames ))
 
 for future in futures:
-  user_row = future.result()
-  print "Row: ", user_row[0].firstname, user_row[0].lastname
+  user_rows = future.result()
+  for row in user_rows:
+    print "Row: ", row.name, row.cost
 
 print "TargetNew: ", archer.getTarget() 
