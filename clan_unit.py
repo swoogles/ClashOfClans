d75338e52e41a7a9811ec23a8462c2a2e95520f1
Bill Frasure <bill.frasure@gmail.com>
2014-05-27 14:51:44 -0600

    Start splitting into multiple files

---
+++ clan_unit.py
@@ -0,0 +1,53 @@
+class Unit():
+
+  def __init__(self, level=1):
+    self.hp_max = self._prop_levels_hp[level]
+    self.hp_cur = self.hp_max 
+    self.dps = self._prop_levels_dps[level]
+    self.cost = self._prop_levels_cost[level]
+    # self.pos = (random.random((0,40)),random.random((0,40)))
+
+  def printStats(self):
+    print "Unit Type: ", self.name
+    print "  hp: ", self.hp_cur
+    print "  dps: ", self.dps
+    print "  cost: ", self.cost
+    print ""
+
+  def printHpCur(self):
+    print "HpCur: ", self.hp_cur
+
+
+  def mapHpLevels(self):
+    newmap = map(double, self._prop_levels_hp.values() )
+    print self._prop_levels_hp
+
+  def printCostLevels(self):
+    self.printLevels( self._prop_levels_cost )
+
+  def printLevels(self, prop_levels):
+    print "Values: ", prop_levels.values()
+
+  def attack(self):
+    self._target.hp_cur -= self.dps
+    if ( self._target.isAlive() == False ):
+      del self._target
+
+  def kill(self):
+    while hasattr( self, '_target' ):
+      self._target.printHpCur()
+      self.attack()
+
+
+  def isAlive(self):
+    return ( self.hp_cur > 0 )
+
+  def setTarget(self, target):
+    self._target = target
+
+  def getTarget(self):
+    return self._target
+
+  def hasTarget(self):
+    return hasattr( self, '_target' )
+
---
+++ units_specific.py
@@ -0,0 +1,61 @@
+from clan_unit import Unit
+class Barbarian(Unit):
+  name = "Barbarian"
+  _range = 1
+
+  _prop_levels_cost = { 
+      1:25,
+      2:40, 
+      3:60,
+      4:80,
+      5:100,
+      6:150
+  }
+
+  _prop_levels_dps = { 
+      1:8,
+      2:11, 
+      3:14,
+      4:18,
+      5:23,
+      6:26,
+  }
+
+  _prop_levels_hp = { 
+      1:45,
+      2:54, 
+      3:65,
+      4:78,
+      5:95,
+      6:110
+  }
+
+  _prop_levels_cost_list = [ 
+      25,
+      40, 
+
+      60,
+      80,
+      100,
+      150
+  ]
+
+
+class Archer(Unit):
+  name = "Archer"
+  _range = 5
+  _prop_levels_hp = { 
+      2:23, 
+      3:28,
+  }
+
+  _prop_levels_dps = { 
+      2:9, 
+      3:12,
+  }
+
+  _prop_levels_cost = { 
+      2:40, 
+      3:80,
+  }
+
