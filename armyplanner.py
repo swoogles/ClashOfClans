2d2cb1e16af2a525c209c36af94ad52e82d99bdd
Bill Frasure <bill.frasure@gmail.com>
2014-07-12 09:41:15 -0600

    Add extremely basic event loop and window for game

--- armyplanner.py
+++ armyplanner.py
@@ -20,6 +20,12 @@ import pygame
 # Initialize the game engine
 pygame.init()
 
+# Define some colors
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+
 class GameBoard():
   width = 40
   height = 40
@@ -124,10 +130,43 @@ copiedBarbarian.kill()
 
 print("TargetedUnit.alive: ", targetedUnit.isAlive() )
 
-for barbarian in unitList:
-  print( "Pos: ", barbarian.reprJSON().get("pos_3d") )
-  print( "Distance: ", copiedBarbarian.distanceFrom(barbarian) ) 
+# for barbarian in unitList:
+#   print( "Pos: ", barbarian.reprJSON().get("pos_3d") )
+#   print( "Distance: ", copiedBarbarian.distanceFrom(barbarian) ) 
 
 # bomb = Bomb(2)
+
+# Opening and setting the window size
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Bill's Cool Game")
+
+# Loop until the user clicks the close button.
+done = False
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+# -------- Main Program Loop -----------
+while not done:
+  # --- Main event loop
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      print("User asked to quit.")
+      done = True # Flag that we are done so we exit this loop
+      # --- Game logic should go here
+      # --- Drawing code should go here
+      # First, clear the screen to white. Don't put other drawing commands
+      # above this, or they will be erased with this command.
+      screen.fill(WHITE)
+      # --- Go ahead and update the screen with what we've drawn.
+      pygame.display.flip()
+      # --- Limit to 60 frames per second
+      clock.tick(60)
+      pygame.quit()
+    elif event.type == pygame.KEYDOWN:
+      print("User pressed a key.")
+    elif event.type == pygame.KEYUP:
+      print("User let go of a key.")
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+      print("User pressed a mouse button")
 
 session.shutdown();
