4c13074526b691c6f11513baef569549f8633b32
Bill Frasure <bill.frasure@gmail.com>
2014-07-13 20:19:13 -0600

    Move database functions to a separate file

--- armyplanner.py
+++ armyplanner.py
@@ -16,6 +16,7 @@ from numpy import array,arange
 from cassandra.cluster import Cluster
 import json
 import itertools
+from database_functions import *
 
 # Import a library of functions called 'pygame'
 import pygame
@@ -48,45 +49,8 @@ class Barracks():
     self.capacity = self._prop_levels_capacity[level]
     self.level = level
 
-def insertUnit(session, targetUnit):
-  table = targetUnit.sql_getTable()
-
-  json = targetUnit.reprJSON()
-  values = json.keys()
-
-  columns = [ val for val in values]
-  columns = str(columns).strip('[]').replace("\'","")
-
-  valueSubs = ['%(' + val + ')s' for val in values]
-
-  valueSubsString = str(valueSubs).strip('[]').replace("\'","")
-
-  _query = " INSERT INTO " + table + " ( " + columns + " ) \
-  VALUES ( " + valueSubsString + ") ";
-
-  
-  session.execute( _query, json)
-
-def resetDB(session, schemaDataFile):
-  # session.execute( cqlCommands)
-  line_number = 0
-  with open('./data.cql', encoding='utf-8') as schemaDataFile:
-    for command in schemaDataFile:
-      # print('{:>4} {}'.format(line_number, command.rstrip()))  
-      session.execute(command.strip())
-      # line_number += 1
-
-
 def printUnit(session, targetUnit):
   print("unit: ", targetUnit.reprJSON() )
-
-def queryAll(session, targetUnit):
-  table = targetUnit.sql_getTable()
-  _query = "SELECT * FROM " + table
-
-  user_rows = session.execute( _query )
-  for row in user_rows:
-    print( "Row: ", row)
 
 curBattle = Battle()
 
---
+++ database_functions.py
@@ -0,0 +1,36 @@
+def insertUnit(session, targetUnit):
+  table = targetUnit.sql_getTable()
+
+  json = targetUnit.reprJSON()
+  values = json.keys()
+
+  columns = [ val for val in values]
+  columns = str(columns).strip('[]').replace("\'","")
+
+  valueSubs = ['%(' + val + ')s' for val in values]
+
+  valueSubsString = str(valueSubs).strip('[]').replace("\'","")
+
+  _query = " INSERT INTO " + table + " ( " + columns + " ) \
+  VALUES ( " + valueSubsString + ") ";
+
+  
+  session.execute( _query, json)
+
+def resetDB(session, schemaDataFile):
+  # session.execute( cqlCommands)
+  line_number = 0
+  with open('./data.cql', encoding='utf-8') as schemaDataFile:
+    for command in schemaDataFile:
+      # print('{:>4} {}'.format(line_number, command.rstrip()))  
+      session.execute(command.strip())
+      # line_number += 1
+
+def queryAll(session, targetUnit):
+  table = targetUnit.sql_getTable()
+  _query = "SELECT * FROM " + table
+
+  user_rows = session.execute( _query )
+  for row in user_rows:
+    print( "Row: ", row)
+
