3edda8b41b4e8788fa3f40be99a56961abf10782
Bill Frasure <bill.frasure@gmail.com>
2014-07-24 16:29:24 -0600

    Fix obvious undefined Class error

--- clan_stationary_unit.py
+++ clan_stationary_unit.py
@@ -5,7 +5,7 @@ class DefensiveUnit(Unit):
     name = "Abstract DefensiveUnit"
 
     def __init__(self, level=1):
-        super(UnitUnit, self).__init__(level)
+        super(Unit, self).__init__(level)
 
     def drawingInfo(self):
         return self.color, (self.pos_3d[0] * 10, self.pos_3d[1] * 10, self.width * 10, self.width * 10), self.fill
