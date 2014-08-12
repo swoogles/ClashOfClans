d51f53122e0c4aa9d1e71181126a4adecde5a87e
Bill Frasure <bill.frasure@gmail.com>
2014-08-11 22:32:40 -0600

    Add br sudocode of neighbor finding algorithm
    Broken because it's not using a provided Queue like I thought, so I have
    to actually link it up with an implemented version. And I'm too tired
    for that right now.

--- graph_tester.py
+++ graph_tester.py
@@ -3,9 +3,22 @@ ug = Graph(directed=False)
 ug.set_directed(False)
 assert(ug.is_directed() == False)
 g1 = Graph()
+v1 = ug.add_vertex()
 v2 = ug.add_vertex()
 e = ug.add_edge(v1, v2)
 graph_draw(ug, vertex_text=ug.vertex_index, vertex_font_size=18, output_size=(200, 200))
 # graph_draw(ug, vertex_text=ug.vertex_index, vertex_font_size=18, output_size=(200, 200), output="two-nodes.png")
 while 1:
-    x=1
+
+    frontier = queue()
+    frontier.put(start)
+    visited = {}
+    visited[start] = True
+
+    while not frontier.empty():
+        print("cool")
+        current = frontier.get()
+        for next in graph.neighbors(current):
+            if next not in visited:
+                frontier.put(next)
+                visited[next] = True
